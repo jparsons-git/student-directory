89fa4ad7795ba4fd0f199717cfe5c119bc1df0f6
jparsons-git <jane.parsons@waitrose.co.uk>
2022-01-20 23:15:23 +0000

    Step8 Excercise5 solution - add more student information

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 # put all the students into an array
 students = [
-  {name: "Dr. Hannibal Lector", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddie Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
+  {name: "Dr. Hannibal Lector", cohort: :november, hobby: "Eating", cob: :USA, height: "6'"},
+  {name: "Darth Vader", cohort: :november, hobby: "Destruction", cob: :USA, height: "5' 8"},
+  {name: "Nurse Ratched", cohort: :november, hobby: "Caring", cob: :Canada, height: "5' 5"},
+  {name: "Michael Corleone", cohort: :november, hobby: "Eating", cob: :England, height: "5' 11"},
+  {name: "Alex DeLarge", cohort: :november, hobby: "Eating", cob: :Denmark, height: "6' 5"},
+  {name: "The Wicked Witch of the West", cohort: :november, hobby: "Flying", cob: :USA, height: "5' 5"},
+  {name: "Terminator", cohort: :november, hobby: "Flying", cob: :Sweden, height: "5' 10"},
+  {name: "Freddie Krueger", cohort: :november, hobby: "Eating", cob: :Denmark, height: "5' 9"},
+  {name: "The Joker", cohort: :november, hobby: "Card Playing", cob: :Canada, height: "4' 9"},
+  {name: "Joffrey Baratheon", cohort: :november, hobby: "Eating", cob: :Sweden, height: "5' 5"},
+  {name: "Norman Bates", cohort: :november, hobby: "Card Playing", cob: :USA, height: "6'"}
 ]
 
 def input_students
@@ -41,7 +41,7 @@ def print(students)
   ind = 0
   while ind < students.count do
     student = students[ind]
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort), #{student[:hobby]}, #{student[:cob]}, #{student[:height]}"
     ind += 1
   end    
 end 
@@ -50,7 +50,8 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
+# Comment out the interactive user input for student for no
+# students = input_students
 print_header
 print(students)
 print_footer(students)
