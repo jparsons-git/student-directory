fca68509dce1dc9e4ff510db5e0d61a15adef959
jparsons-git <jane.parsons@waitrose.co.uk>
2022-01-23 23:35:30 +0000

    Initial commit for Step10 - refactoring

--- directory.rb
+++ directory.rb
@@ -1,19 +1,24 @@
+# Refactoring exercise
+# For the puroses of this exercise, removing dynamic column widths, default cohort selection, the original "given" students array
+# And for now, showing the students by cohorts... although I will plug in a way to do this using the new method show_students
 
+@students = []
+@col_titles = ["Name", "Cohort", "Hobby", "Country of birth", "Height"]
+@col_widths = [45, 20, 20, 20, 10]  # for the purpose of refactoring, use static column lengths
+@cohorts = [
+  {cohort: :january, default: false}, 
+  {cohort: :march, default: true}, 
+  {cohort: :may, default: false}, 
+  {cohort: :july, default: false}, 
+  {cohort: :september, default: false}, 
+  {cohort: :november, default: false}
+  ]
+  
 def input_students(months)
-  puts "Please enter the names of the students"
-  puts "To finish, just hot return twice"
-  # initialise an empty students array
-  students = []
-  
-  # get the first name using chomp
-  # name = gets.chomp
-  # get the first name using gets - ie String - For step8 Exercise10
-  name = gets
-  name = name.slice(0,name.length-1) # remove last charcter (new line)
-  
-  # while name is not empty... repeat 
+  puts "Please enter the name(s) of the student(s). To finish, just hit return twice"
+  students = @students
+  name = gets.chomp
   while !name.empty? do
-    
     # select the cohort
     valid = false
     chosen_month = ""
@@ -23,10 +28,10 @@ def input_students(months)
       months.each do |month|
         # output the options, and derive the default month
         if month[:default]
-          puts "#{month[:cohort]} (default month)"
+          puts "      #{month[:cohort]} (default month)"
           default_month = month[:cohort]
         else
-          puts "#{month[:cohort]}"
+          puts "      #{month[:cohort]}"
         end  
       end
       # get/map the availble months for the cohorts
@@ -35,10 +40,9 @@ def input_students(months)
       if months_available.include? (chosen_month.to_sym)  
         valid = true
       elsif chosen_month == ""
-        valid = true
-        # use the default month here
+        valid = true # use the default month here
         chosen_month = default_month
-        puts "use the default month of #{default_month}"
+        puts "#{default_month} (default) selected"
       else
         puts "#{chosen_month} is NOT ncluded in the above list - please try again"
       end 
@@ -51,13 +55,13 @@ def input_students(months)
       hobby = gets.chomp
       if hobby != ""
         valid = true
-      end  
-    end
-    
-    # enter Country of Birth
+      end
+    end    
+     
+    # enter Country of birth  
     valid = false
     while !valid do
-      puts "Please enter your Counttry of Birth"
+      puts "Please enter your Country of Birth"
       cob = gets.chomp
       if cob != ""
         valid = true
@@ -76,184 +80,23 @@ def input_students(months)
     
     students << {name: name, cohort: chosen_month.to_sym, hobby: hobby, cob: cob, height: height}
     puts "Now we have #{students.count} students"
-    # get another name from the user
-    name = gets.chomp
+    puts "Please enter the name of the another student, or to finish just hit enter"
+    name = gets.chomp  # get another name from the user or finish 
   end
-  # finally return the array of students
-  return students
-end 
-
-def select_on_cohort(students)
-  # firstly find the distinct cohort values
-  cohorts_available = students.map { |student| student[:cohort] }.uniq 
-  valid = false
-  
-  while !valid do
-    puts "Please select the cohort from the following list"
-    cohorts_available.each do |option|
-      puts option
-    end  
-    cohort = gets.chomp
-    if cohorts_available.include? (cohort.to_sym)  
-      valid = true
-    else
-      puts "#{cohort} is NOT ncluded in the above list - please try again"
-    end  
-  end
-  # todo I feel sure this can be done more succinctly without the "each" loop - come back to this
-  selected_students = []
-  students.each do |student|
-    if student[:cohort].to_s == cohort
-      selected_students.push(student)
-    end  
-  end 
-  return selected_students
-end 
-
-def print_by_cohort(students, widths)
-  # firstly find the distinct cohort values
-  cohorts_available = students.map { |student| student[:cohort] }.uniq
-  
-  grouped_students = []
-  cohorts_available.each do |cohort|
-    #puts "THIS IS THE COHORT #{cohort}"
-    grouped_students = []
-    students.each do |student|
-      #puts "THIS IS THE STUDENT'S COHORT #{student[:cohort]}"
-      if student[:cohort] == cohort
-        #puts "DO WE HAVE A MATCH on #{cohort}"
-        grouped_students.push(student)
-      end
-    end
-    print(grouped_students, widths)
-    puts grouped_students.count > 1 ? "In the #{cohort} cohort, we have #{grouped_students.count} great students".center(find_total_width(students), ' ')
-                                    : "In the #{cohort} cohort, we have #{grouped_students.count} great student".center(find_total_width(students), ' ')
-    #puts "In the #{cohort} cohort, we have #{grouped_students.count} great students".center(find_total_width(students), ' ') :
-    puts  
-  end  
-end  
-
-def print_header(students)
-  puts "The students of Villains Academy".center(find_total_width(students), ' ')
-  puts "--------------------------------".center(find_total_width(students), ' ')
-end
-
-def print(students, widths)
-  name_col = "NAME"
-  cohort_col = "COHORT"
-  hobby_col = "HOBBY"
-  cob_col = "COUNTRY"
-  # output the column headings
-  puts "#{name_col.center(widths[0], ' ')} #{cohort_col.center(18, ' ')} #{hobby_col.center(widths[2], ' ')} #{cob_col.center(widths[3], ' ')} HEIGHT"
- # and now all the student information  
-  ind = 0
-  while ind < students.count do
-    student = students[ind]
-    # build the cohort string inbetween brackets with the static word "cohort"
-    cohort = "(#{student[:cohort].to_s} cohort)"
-    puts "#{student[:name].center(widths[0], ' ')} #{cohort.center(18, ' ')} #{student[:hobby].center(widths[2], ' ')} #{student[:cob].to_s.center(widths[3], ' ')} #{student[:height]}"
-    ind += 1
-  end    
-end
-
-def print_footer(names)
-  puts names.count > 1 ? "Overall, we have #{names.count} great students".center(find_total_width(names), ' ')
-                       : "Overall, we have #{names.count} great student".center(find_total_width(names), ' ')
-end
-
-def find_column_width(students)
-  # find the maximum width of each column from the contents
-  max_name = 4
-  max_hobby = 5
-  max_cob = 7
-  students.each do |student|
-    if student[:name].length > max_name
-      max_name = student[:name].length
-    end
-    if student[:hobby].length > max_hobby
-      max_hobby = student[:hobby].length
-    end
-    if student[:cob].length > max_cob
-      max_cob = student[:cob].length
-    end  
-  end
-  # hardcode the width for the COHORT column and the HEIGHT column as is constant
-  return [max_name, 18, max_hobby, max_cob, 6]
-end  
-
-
-def find_total_width(students)
-  # derive the total width of the table of information - so enable centralising it
-  total_width = 0
-  find_column_width(students).each do |width|
-    total_width += width + 1
-  end
-  return total_width
+  return students  # finally return the array of students
 end 
 
 def interactive_menu
-  
-  given_students = [
-  {name: "Dr. Hannibal Lector", cohort: :november, hobby: "Eating", cob: :USA, height: "6'"},
-  {name: "Darth Vader", cohort: :november, hobby: "Destruction", cob: :USA, height: "5' 8"},
-  {name: "Nurse Ratched", cohort: :may, hobby: "Caring", cob: :Canada, height: "5' 5"},
-  {name: "Michael Corleone", cohort: :november, hobby: "Eating", cob: :England, height: "5' 11"},
-  {name: "Alex DeLarge", cohort: :september, hobby: "Eating", cob: :Denmark, height: "6' 5"},
-  {name: "The Wicked Witch of the West", cohort: :november, hobby: "Flying", cob: :USA, height: "5' 5"},
-  {name: "Terminator", cohort: :may, hobby: "Flying", cob: :Sweden, height: "5' 10"},
-  {name: "Freddie Krueger", cohort: :november, hobby: "Eating", cob: :Australia, height: "5' 9"},
-  {name: "The Joker", cohort: :may, hobby: "Card Playing", cob: :Canada, height: "4' 9"},
-  {name: "Joffrey Baratheon", cohort: :november, hobby: "Eating", cob: :Sweden, height: "5' 5"},
-  {name: "Norman Bates", cohort: :november, hobby: "Card Playing", cob: :USA, height: "6'"}
-  ]
-
-# Uncomment the predefined cohort choices for user selection
-  cohorts = [
-  {cohort: :january, default: false}, 
-  {cohort: :march, default: true}, 
-  {cohort: :may, default: false}, 
-  {cohort: :july, default: false}, 
-  {cohort: :september, default: false}, 
-  {cohort: :november, default: false}
-  ]
-  
-  students = []
+  students = @students
   loop do
-    # list the options for the user
-    puts "1. Input students"
-    puts "2. Show students"
-    puts "3. Show given students"
-    puts "4. Show given students by cohort"
-    puts "9. Exit"
-    # get the user selected option
+    print_menu # list the options for the user
     selected = gets.chomp
-    # put into action the user's selection
-    case selected
+    case selected # now put into action the user's selection
     when "1"
-      students = input_students(cohorts)
+      students = input_students(@cohorts)
     when "2"
       if students.count > 0
-        widths = []
-        widths=find_column_width(students) # get the widths dynamically according to full list od students 
-        print_header(students)
-        print(students, widths)
-        print_footer(students)
-      end
-    when "3"
-      if given_students.count > 0
-        widths = []
-        widths=find_column_width(given_students) # get the widths dynamically according to full list od students 
-        print_header(given_students)
-        print(given_students, widths)
-        print_footer(given_students)
-      end
-    when "4"
-      if given_students.count > 0
-        widths = []
-        widths=find_column_width(given_students) # get the widths dynamically according to full list od students 
-        print_header(given_students)
-        print_by_cohort(given_students, widths)
-        print_footer(given_students)
+        show_students(students)
       end
     when "9"
       exit
@@ -262,3 +105,46 @@ def interactive_menu
     end  
   end  
 end 
+
+def print_menu
+  puts "1. Input students"
+  puts "2. Show students"
+  #puts "3. Show given students"
+  #puts "4. Show given students by cohort"
+  puts "9. Exit"
+end  
+
+def show_students(students)
+  print_header
+  print(students)
+  print_footer(students)
+end  
+
+def print_header()
+  puts"start header"
+  total_width = 0
+  @col_widths.each do |width| total_width += width end
+  puts "The students of Villains Academy".center(total_width, ' ')
+  puts "--------------------------------".center(total_width, ' ')
+end
+
+def print(students)
+  # output the column headings
+  puts "#{@col_titles[0].center(@col_widths[0], ' ')} #{@col_titles[1].center(@col_widths[1], ' ')} #{@col_titles[2].center(@col_widths[2], ' ')} #{@col_titles[3].center(@col_widths[3], ' ')} #{@col_titles[4]}"
+ # and now all the student information  
+  ind = 0
+  while ind < students.count do
+    student = students[ind]
+    # build the cohort string inbetween brackets with the static word "cohort"
+    cohort = "(#{student[:cohort].to_s} cohort)"
+    puts "#{student[:name].center(@col_widths[0], ' ')} #{cohort.center(@col_widths[1], ' ')} #{student[:hobby].center(@col_widths[2], ' ')} #{student[:cob].to_s.center(@col_widths[3], ' ')} #{student[:height]}"
+    ind += 1
+  end    
+end
+
+def print_footer(names)
+  total_width = 0
+  @col_widths.each do |width| total_width += width end
+  puts names.count > 1 ? "Overall, we have #{names.count} great students".center(total_width, ' ')
+                       : "Overall, we have #{names.count} great student".center(total_width, ' ')
+end
